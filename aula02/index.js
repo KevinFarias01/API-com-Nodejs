3947dafe74871a4aeb4e77947f16136291fee1f7
Kevin Farias <kevin.farias.rib@gmail.com>
2022-06-19 17:58:13 -0300

    1

---
+++ aula02/index.js
@@ -0,0 +1,17 @@
+/*
+    0- Obter usuário
+    1- Obter tel do usuário pelo ID
+    2- Obter endereço do usuário pelo ID
+*/
+
+function obterUsuario() {
+
+}
+
+function obterTelefone(idUsuario) {
+
+}
+
+function obterEndereco(idUsuario) {
+
+}
